36c1089703119b7bb7b6e59625cfc4ba12a101f4
Raj Kumar Kuncham <rajudruva2@gmail.com>
2026-06-02 17:03:59 +0000

    Initial Backstage commit

---
+++ packages/app/src/App.tsx
@@ -0,0 +1,42 @@
+
+
+
+import { createApp } from '@backstage/frontend-defaults';
+import catalogPlugin from '@backstage/plugin-catalog/alpha';
+import githubActionsPlugin from '@backstage-community/plugin-github-actions/alpha';
+import { navModule } from './modules/nav';
+import { githubAuthApiRef } from '@backstage/core-plugin-api';
+import { SignInPageBlueprint } from '@backstage/plugin-app-react';
+import { SignInPage } from '@backstage/core-components';
+import { createFrontendModule } from '@backstage/frontend-plugin-api';
+
+const signInPage = SignInPageBlueprint.make({
+  params: {
+    loader: async () => props =>
+      (
+        <SignInPage
+          {...props}
+          provider={{
+            id: 'github-auth-provider',
+            title: 'GitHub',
+            message: 'Sign in using GitHub',
+            apiRef: githubAuthApiRef,
+          }}
+        />
+      ),
+  },
+});
+
+export default createApp({
+  features: [
+    catalogPlugin,
+    githubActionsPlugin, 
+    navModule,
+    createFrontendModule({
+      pluginId: 'app',
+      extensions: [signInPage],
+    }),
+  ],
+});
+
+
